c15a2da7f8304bc8309202e040162778cc09f318
SeungHyuckJeon <sdkazx2717@gmail.com>
2021-08-06 12:57:55 +0900

    project initialize

---
+++ restsample_kotlin/src/main/kotlin/com/example/restsample/RestsampleApplication.kt
@@ -0,0 +1,11 @@
+package com.example.restsample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RestsampleApplication
+
+fun main(args: Array<String>) {
+	runApplication<RestsampleApplication>(*args)
+}
